5401fb8c5dabed66d206539fde8e30fad6b6123b
Antonio Cacio <72471479+CacioA@users.noreply.github.com>
2022-11-06 15:54:34 +0000

    TA-11 added default values to getJsonResults

--- src/main/java/com/example/apexf1/controller/ResultController.java
+++ src/main/java/com/example/apexf1/controller/ResultController.java
@@ -6,10 +6,12 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/f1")
 public class ResultController {
 
     @Autowired
@@ -20,8 +22,8 @@ public class ResultController {
 //  Get race results based on year and race number
     @GetMapping("/results")
     public JsonNode getJsonResults(
-            @RequestParam(name="year") String year,
-            @RequestParam(name="raceNum") String raceNum) throws JsonProcessingException {
+            @RequestParam(name="year", defaultValue = "2022") String year,
+            @RequestParam(name="raceNum", defaultValue = "1") String raceNum) throws JsonProcessingException {
 
         String result = resultService.findResultWithYearAndRaceNumber(year,raceNum);
 
